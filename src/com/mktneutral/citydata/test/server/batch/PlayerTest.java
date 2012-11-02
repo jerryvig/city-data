2f82d8011b6bda0e1fa317070ee5399310ba9327
unknown <JVigil0515c@CACVSAL-5LTW0F1.cable.comcast.com>
2012-11-02 15:44:06 -0700

    adding more unit tests

--- src/com/mktneutral/citydata/test/server/batch/PlayerTest.java
+++ src/com/mktneutral/citydata/test/server/batch/PlayerTest.java
@@ -11,8 +11,30 @@ public class PlayerTest {
   public void testGetName() {
 	  Player player1 = new Player("FluviaLacerda");
 	  Player player2 = new Player("JessicaKane");
+	  Player player3 = new Player("TessMunster");
 	  
 	  Assert.assertEquals(player1.getName(),"FluviaLacerda");
 	  Assert.assertEquals(player2.getName(),"JessicaKane");
+	  Assert.assertEquals(player3.getName(), "TessMunster");
+  }
+  
+  @Test
+  public void testGetScore() {
+	  Player player1 = new Player("RosieMercado");
+	  Assert.assertEquals(player1.getScore(), 0);
+	  
+	  player1.incrementScore(20);
+	  Assert.assertEquals(player1.getScore(),20);
+	  
+	  player1.incrementScore(50);
+	  Assert.assertEquals(player1.getScore(), 70);
+	  
+	  player1.incrementScore(100);
+	  Assert.assertEquals(player1.getScore(), 170);
+  }
+  
+  @Test
+  public void testDealLetters() {
+	  
   }
 }
